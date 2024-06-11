2302af6cc9dab0e8d615c495336c47ce8a7ff8df
kevin-ada <kevinkoech265@gmail.com>
2024-06-11 23:27:10 +0300

    making some changes to the API

--- timestamp/api/views.py
+++ timestamp/api/views.py
@@ -8,5 +8,4 @@ class GenerateTitlesView(View):
     def get(self, request, video_id):
         # Always generate the chapter titles and store them in the database
         titles = generate_chapter_titles(video_id)
-        Video.objects.create(video_id=video_id, chapter_titles=titles)
         return JsonResponse(titles, safe=False)
