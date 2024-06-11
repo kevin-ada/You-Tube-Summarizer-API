5469dbbda13f74daeaaa715d473499f3b4bbe3af
kevin-ada <kevinkoech265@gmail.com>
2024-06-11 23:01:01 +0300

    making some changes to the API

--- timestamp/api/views.py
+++ timestamp/api/views.py
@@ -6,13 +6,7 @@ from timestamp.models import Video
 
 class GenerateTitlesView(View):
     def get(self, request, video_id):
-        # Check if video_id already exists in the database
-        video = Video.objects.filter(video_id=video_id).first()
-        if video:
-            # If it exists, return the stored chapter titles
-            titles = video.chapter_titles
-        else:
-            # If it doesn't exist, generate the chapter titles and store them in the database
-            titles = generate_chapter_titles(video_id)
-            Video.objects.create(video_id=video_id, chapter_titles=titles)
+        # Always generate the chapter titles and store them in the database
+        titles = generate_chapter_titles(video_id)
+        Video.objects.create(video_id=video_id, chapter_titles=titles)
         return JsonResponse(titles, safe=False)
